3d107a420bf6db6fd2ef2d7a4f08043c94439b58
Maazin Sherif <maazinsherif99@gmail.com>
2023-10-05 18:48:10 -0400

    first few problems

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,18 @@
-// Your code here
+class Dragon {
+  static getDragons(...dragons) {
+    return dragons.map(dragon => dragon.name); //takes object of dragons and takes names and puts into array
+  }
+  
+  constructor(name, color){
+    this.name = name;
+    this.color = color;
+  }
+  breathesFire() {
+    return (`${this.name} breathes fire everywhere! BURN!!!!`)
+  }
+}
+
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
